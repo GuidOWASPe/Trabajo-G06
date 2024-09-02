f8240d8bab5c6c58998d584a116b495f671c256d
Guido Navarro <33993228+MollyKewl@users.noreply.github.com>
2024-09-02 12:30:45 -0500

    para mejorar las carpetas
    xd

--- StyleCheck/demo/src/main/java/pe/edu/upc/demo/repositories/IEstiloItemRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.demo.entities.EstiloItem;
-
-public interface IEstiloItemRepository extends JpaRepository<EstiloItem, Integer> {
-}
--- StyleCheck/demo/src/main/java/pe/edu/upc/demo/repositories/IItemUsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.demo.entities.ItemUsuario;
-
-@Repository
-public interface IItemUsuarioRepository extends JpaRepository<ItemUsuario, Integer> {
-}
