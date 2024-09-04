d056b90816b14ba22a535fc756f70c15f7b3ed70
GsuS2004 <U20221A615@upc.edu.pe>
2024-09-04 03:18:37 -0500

    Agregue mis reportes
    Reporte 1: Formas de rostro más populares
    
    Reporte 2: Ver las combinaciones de formas del rostro y colores de estilos que son más populares

--- demo/src/main/java/pe/edu/upc/demo/controllers/RostroController.java
+++ demo/src/main/java/pe/edu/upc/demo/controllers/RostroController.java
@@ -3,10 +3,13 @@ package pe.edu.upc.demo.controllers;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.demo.dtos.CantidadEstiloColorFormaDTO;
+import pe.edu.upc.demo.dtos.CantidadRostroFormaDTO;
 import pe.edu.upc.demo.dtos.RostroDTO;
 import pe.edu.upc.demo.entities.Rostro;
 import pe.edu.upc.demo.serviceinterfaces.IRostroService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -50,5 +53,18 @@ public class RostroController {
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable("id") Integer id){
         rS.delete(id);
+    }
+
+    @GetMapping("/rostrosformas")
+    public List<CantidadRostroFormaDTO> cantidadRostrosForma(){
+        List<String[]>lista= rS.cantidadRostrosForma();
+        List<CantidadRostroFormaDTO> listaDTO = new ArrayList<>();
+        for(String[] columna:lista){
+            CantidadRostroFormaDTO dto=new CantidadRostroFormaDTO();
+            dto.setNombreForma(columna[0]);
+            dto.setCantidadRostro(Integer.parseInt(columna[1]));
+            listaDTO.add(dto);
+        }
+        return listaDTO;
     }
 }
--- demo/src/main/java/pe/edu/upc/demo/repositories/IEstiloRepository.java
+++ demo/src/main/java/pe/edu/upc/demo/repositories/IEstiloRepository.java
@@ -1,8 +1,26 @@
 package pe.edu.upc.demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.demo.entities.Estilo;
+
+import java.util.List;
+
 @Repository
 public interface IEstiloRepository extends JpaRepository<Estilo,Integer> {
+
+    @Query(value = " SELECT \n" +
+            "    F.nombre_forma AS Nombre_Forma,\n" +
+            "    C.nombre_color AS Codigo_Color,\n" +
+            "    COUNT(E.id_estilo) AS Cantidad_Estilos\n" +
+            " FROM     Estilo E\n" +
+            " LEFT JOIN Rostro R\n" +
+            " ON E.id_rostro = R.id_rostro\n" +
+            " LEFT JOIN Forma F\n" +
+            " ON R.id_forma = F.id_forma\n" +
+            " RIGHT JOIN Color C\n" +
+            " ON E.id_color = C.id_color\n" +
+            " GROUP BY (F.nombre_forma,C.nombre_color)", nativeQuery = true)
+    public List<String[]> cantidadEstiloColorForma();
 }
