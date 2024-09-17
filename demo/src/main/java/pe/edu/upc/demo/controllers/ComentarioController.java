be790826e880843ac8b9567daaa72d3f1f6de3b8
Miguelcgcode <u20201f517@upc.edu.pe>
2024-09-17 01:42:21 -0500

    Cambio nombre de ComentarioController funcion

--- demo/src/main/java/pe/edu/upc/demo/controllers/ComentarioController.java
+++ demo/src/main/java/pe/edu/upc/demo/controllers/ComentarioController.java
@@ -53,7 +53,7 @@ public class ComentarioController {
         cT.delete(id);
     }
 
-    @GetMapping("/comentariosnegativos")
+    @GetMapping("/ContadorChatsNegativos")
     public List<ComentariosNegativosFrecuentesDTO> listarComentariosNegativosFrecuentes(){
         List<String[]>lista= cT.listarComentariosNegativosFrecuentes();
         List<ComentariosNegativosFrecuentesDTO> listaDTO = new ArrayList<>();
