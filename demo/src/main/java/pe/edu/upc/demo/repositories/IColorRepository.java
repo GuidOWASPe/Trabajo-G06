7a7925bf6144fc8ffafda98a7cd5064651af203d
Guido Navarro <33993228+MollyKewl@users.noreply.github.com>
2024-10-05 18:08:01 -0500

    vero1

--- demo/src/main/java/pe/edu/upc/demo/repositories/IColorRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.demo.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.demo.entities.Color;
-
-@Repository
-public interface   IColorRepository extends JpaRepository<Color, Integer> {
-}
